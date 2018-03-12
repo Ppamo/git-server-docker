9dfb93c7ca759ba23457b45c7f492db31070e664
Pablo Mansilla Ojeda <pablo@ppamo.cl>
2018-03-12 11:44:17 -0300

    Create new method to deploy and undeploy and change namespace from default to utils

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@ GREEN='\e[92m'
 BLUE='\e[36m'
 YELLOW='\e[93m'
 RESET='\e[39m'
-IMAGENAME=${IMAGENAME:=git-server-docker}
+IMAGENAME=${IMAGENAME:=default/git-server}
 IMAGEVERSION=${IMAGEVERSION:=0.1.0}
-IMAGETAG=${IMAGETAG:=$IMAGENAME:$IMAGEVERSION}
+REGISTRY=${REGISTRY:=localhost}
+IMAGETAG=${IMAGETAG:=$REGISTRY/$IMAGENAME:$IMAGEVERSION}
+DEPLOYFILE=resources/kubernetes/git_server.de.yaml
+PAYLOAD="git_server.pv.yaml git_server.pvc.yaml git_server.se.yaml"
 
 usage(){
 	printf $YELLOW"usage:
@@ -38,10 +41,40 @@ list(){
 	docker images $IMAGETAG --format "{{.ID}} - {{.Repository}}:{{.Tag}} - {{printf \"%.20s\" .CreatedAt}}"
 }
 
+deploy(){
+	printf $YELLOW"+ deploying to $REGISTRY\n"$RESET
+	kubectl create -f $DEPLOYFILE
+}
+
+undeploy(){
+	printf $YELLOW"+ undeploying from $REGISTRY\n"$RESET
+	kubectl delete -f $DEPLOYFILE
+}
+
+deploypayload(){
+	for RESOURCE in $PAYLOAD
+	do
+		printf $YELLOW"+ deploying $RESOURCE to $REGISTRY\n"$RESET
+		kubectl create -f resources/kubernetes/$RESOURCE
+	done
+}
+
+undeploypayload(){
+	for RESOURCE in $PAYLOAD
+	do
+		printf $YELLOW"+ undeploying $RESOURCE to $REGISTRY\n"$RESET
+		kubectl delete -f resources/kubernetes/$RESOURCE
+	done
+}
+
 case $1 in
-	build)		build		;;
-	clean)		clean		;;
-	list)		list		;;
+	build)			build			;;
+	clean)			clean			;;
+	list)			list			;;
+	deploy)			deploy			;;
+	deploypayload)		deploypayload		;;
+	undeploy)		undeploy		;;
+	undeploypayload)	undeploypayload		;;
 	*)
 		usage
 		exit 0
